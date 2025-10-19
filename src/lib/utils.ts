57796bca14b07d89f441f028af343baa499c2dc7
Paulo Sergio <contato.paulofws@gmail.com>
2025-10-19 18:10:34 -0300

    feat: add formatGenre function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
+import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const formatGenre = (genre: string) => {
+  switch (genre) {
+    case "male":
+      return "Masculino"
+    case "female":
+      return "Feminino"
+    default:
+      return "Outro"
+  }
 }
