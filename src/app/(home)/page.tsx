ff602c56d8e96f52082b0b3e32c781823ad2d91e
Paulo Sergio <contato.paulofws@gmail.com>
2025-10-21 21:37:17 -0300

    feat: update home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,13 @@
+import { FeatureSection } from "./_components/features-section"
+import { HeroCarousel } from "./_components/hero-carousel"
+import { MostPopularSection } from "./_components/most-popular-section"
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="font-primary font-bold text-6xl text-center">FlexShop</h1>
-    </div>
+    <main className="flex flex-col flex-1 min-h-[calc(100dvh-72px)]">
+      <HeroCarousel />
+      <FeatureSection />
+      <MostPopularSection />
+    </main>
   )
 }
