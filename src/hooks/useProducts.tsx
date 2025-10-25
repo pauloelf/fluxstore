de5114ea0db29931108ad4d300537261120fa117
Paulo Sergio <contato.paulofws@gmail.com>
2025-10-25 18:05:14 -0300

    refactor: update useProducts hook

--- src/hooks/useProducts.tsx
+++ src/hooks/useProducts.tsx
@@ -1,11 +1,44 @@
 import { type UseQueryResult, useQuery } from "@tanstack/react-query"
-import type { ProductParams } from "@/@types/product-types"
+import type { Filters } from "@/@types/filters-types"
 import api from "@/lib/api"
 
-const fetchProducts = async ({ limit, order, skip, sortBy }: ProductParams) => {
+const fetchProducts = async ({
+  order,
+  page,
+  sortBy,
+  category,
+  search,
+  limit,
+}: Filters) => {
+  const limitValue = limit || 16
+  const skip = ((page ?? 1) - 1) * limitValue
+
+  if (search && !category) {
+    const response = await api.get("/products/search", {
+      params: {
+        limit: limitValue,
+        skip,
+        sortBy,
+        order,
+        q: search,
+      },
+    })
+    return response.data
+  } else if (category) {
+    const response = await api.get(`/products/category/${category}`, {
+      params: {
+        limit: limitValue,
+        skip,
+        sortBy,
+        order,
+      },
+    })
+    return response.data
+  }
+
   const response = await api.get("/products", {
     params: {
-      limit,
+      limit: limitValue,
       skip,
       sortBy,
       order,
@@ -15,19 +48,25 @@ const fetchProducts = async ({ limit, order, skip, sortBy }: ProductParams) => {
 }
 
 export function useProducts<T>({
-  limit,
   order,
-  skip,
+  page,
   sortBy,
-}: ProductParams): UseQueryResult<
+  category,
+  search,
+  limit,
+}: Filters): UseQueryResult<
   {
     products: T
+    total: number
+    skip: number
+    limit: number
   },
   Error
 > {
   return useQuery({
-    queryFn: () => fetchProducts({ limit, order, skip, sortBy }),
-    queryKey: ["products", sortBy, order, limit, skip],
+    queryFn: () =>
+      fetchProducts({ order, page, sortBy, category, search, limit }),
+    queryKey: ["products", sortBy, order, page, category, search, limit],
     staleTime: 1000 * 60 * 5,
     retry: 1,
     refetchOnWindowFocus: false,
