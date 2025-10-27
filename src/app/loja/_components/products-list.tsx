4150d057ff1d20f251059e2826274f1e86f85f6e
Paulo Sergio <contato.paulofws@gmail.com>
2025-10-26 21:15:23 -0300

    feat: add product-list component

---
+++ src/app/loja/_components/products-list.tsx
@@ -0,0 +1,63 @@
+"use client"
+
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+import { useRef } from "react"
+import type { Product } from "@/@types/product-types"
+import { LoadingSpinner } from "@/components/shared/loading-spinner"
+import { ProductCard } from "@/components/shared/product-card"
+import { useFilters } from "@/hooks/useFilters"
+import { useProducts } from "@/hooks/useProducts"
+import { getTotalPages } from "@/lib/utils"
+import { LojaFilters } from "./loja-filters"
+import { LojaPagination } from "./loja-pagination"
+
+export function ProductsList() {
+  const { filters, setFilters } = useFilters()
+  const { data, isLoading } = useProducts<Product[]>(filters)
+  const { total } = getTotalPages(data?.total ?? 0)
+  const containerRef = useRef(null)
+
+  useGSAP(
+    () => {
+      if (!data?.products) return
+
+      const tl = gsap.timeline({
+        defaults: { duration: 0.3, ease: "power2.out" },
+      })
+
+      const cards = gsap.utils.toArray(".product-card")
+
+      tl.from(cards, { opacity: 0, y: 100, stagger: 0.1 })
+    },
+    { scope: containerRef, dependencies: [data?.products] },
+  )
+
+  return (
+    <section className="scroll-smooth">
+      <LojaFilters filters={filters} setFilters={setFilters} />
+      <div ref={containerRef}>
+        {isLoading ? (
+          <LoadingSpinner message="Carregando produtos..." />
+        ) : (
+          <div className="gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 py-6">
+            {data?.products?.map((product) => (
+              <ProductCard
+                className="product-card"
+                key={product.id}
+                product={product}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+      {!isLoading && (
+        <LojaPagination
+          filters={filters}
+          setFilters={setFilters}
+          total={total}
+        />
+      )}
+    </section>
+  )
+}
